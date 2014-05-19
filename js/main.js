7dc5751e3d264507e5f60a48a894c2a2badfc4dc
bcolb <brice.tech@gmail.com>
2014-05-18 22:36:37 -0400

    Part 5 - Using Handlebar Templates

--- js/main.js
+++ js/main.js
@@ -1,32 +1,35 @@
 var app = {
 
     showAlert: function(message, title) {
+	// Message modification for troubleshooting purposes
 	if (navigator.notification) {
+	    message = "Native Notification " + message;
 	    navigator.notification.alert(message, null, title, 'OK');
 	} else {
+	    message = "Browser Notification " + message;
 	    alert(title ? (title + ": " + message) : message);
 	}
     },
 
+    renderHomeView: function() {
+	$('body').html(this.homeTpl());
+	$('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+
     findByName: function() {
-        console.log('findByName');
+	var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+            $('.employee-list').html(self.employeeLiTpl(employees));
         });
     },
 
     initialize: function() {
 	var self = this;
+	this.homeTpl = Handlebars.compile($("#home-tpl").html());
+	this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
         this.store = new MemoryStore(function() {
-	    self.showAlert('Store Initialized', 'Info');
+	    self.renderHomeView();
 	});
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
 };
