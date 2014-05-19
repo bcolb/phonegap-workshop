5993cad8e24eda4cfaf2353fa2dc8753b8bb357e
bcolb <brice.tech@gmail.com>
2014-05-19 08:02:36 -0400

    Part 8 - Highlighting Tapped or Clicked UI Elements

--- js/main.js
+++ js/main.js
@@ -11,11 +11,31 @@ var app = {
 	}
     },
 
+    registerEvents: function() {
+	var self = this;
+	// Check of browser supports touch events
+	if (document.documentElement.hasOwnProperty('ontouchstart')) {
+	    // ... if yes: register touch even listener to change the "selected" state of the item
+	    $('body').on('touchstart', 'a', function(even) {
+		$(event.target).removeClass('tappable-active');
+	    });
+	} else {
+	    // ... if not: register mouse events instead
+	    $('body').on('mousedown', 'a', function(event) {
+		$(event.target).addClass('tappable-active');
+	    });
+	    $('body').on('mouseup', 'a', function(event) {
+		$(event.target).removeClass('tappable-active');
+	    });
+	}
+    },
+
     initialize: function() {
 	var self = this;
         this.store = new MemoryStore(function() {
 	    $('body').html(new HomeView(self.store).render().el);
 	});
+	this.registerEvents();
     }
 
 };
